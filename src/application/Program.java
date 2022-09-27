35eb50d26d6844764b26f5e90b2144824e32a655
Leianny-Poiani <leianny.poiani@ibm.com>
2022-09-27 10:54:27 -0300

    Seller entity created

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,19 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.sql.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
-
         System.out.println(obj);
 
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com",new Date(12/12/1998), 3000.00, obj);
+        System.out.println(seller);
     }
 }
